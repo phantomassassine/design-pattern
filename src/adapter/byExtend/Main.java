b464734dac67b230a344ca11a4d79c3839edd16d
phantomassassine <r18951111497@163.com>
2024-07-15 12:33:31 +0800

    adaptation

---
+++ src/adapter/byExtend/Main.java
@@ -0,0 +1,13 @@
+package adapter.sample1;
+
+/**
+ * Main中使用Print接口进行编程，即调用Print.printWeak和Print.printStrong方法。
+ * 对Main而言Banner类的showWithParen和showWithAster方法都被完全隐藏起来了。
+ */
+public class Main {
+    public static void main(String[] args) {
+        Print p = new PrintBanner("Hello");
+        p.printWeak();
+        p.printStrong();
+    }
+}
---
+++ src/adapter/byExtend/PrintBanner.java
@@ -0,0 +1,17 @@
+package adapter.sample1;
+
+/**
+ * 适配器类
+ * 继承了Banner类，实现了Print接口。
+ */
+public class PrintBanner extends Banner implements Print {
+    public PrintBanner(String string) {
+        super(string);
+    }
+    public void printWeak() {
+        showWithParen();
+    }
+    public void printStrong() {
+        showWithAster();
+    }
+}
